ca80e658633c422a4da7b9bed8ae7fee8af03491
cmf028 <cmf1292@hotmail.com>
2013-06-06 13:27:07 -0500

    Add helper classes for converting between recast/detour and quake3 coordinates

--- tools/quake3/q3map2/navgen.h
+++ tools/quake3/q3map2/navgen.h
@@ -101,17 +101,3 @@ const float           *getVerts() { return verts; }
 const int              getNumVerts() { return nverts; }
 const rcChunkyTriMesh *getChunkyMesh() { return &mesh; }
 };
-
-static void quake2recast( float vec[ 3 ] ){
-	float temp = vec[ 1 ];
-	vec[ 0 ] = -vec[ 0 ];
-	vec[ 1 ] = vec[ 2 ];
-	vec[ 2 ] = -temp;
-}
-
-static void recast2quake( vec3_t vec ){
-	vec_t temp = vec[1];
-	vec[0] = -vec[0];
-	vec[1] = -vec[2];
-	vec[2] = temp;
-}
