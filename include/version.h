8be92adf1a650f702cb95199829c32015f7c7b65
Thomas Debesse <dev@illwieckz.net>
2019-06-05 02:00:58 +0200

    cmake/makefile: there is no need for MAJOR/MINOR/PATCH anymore

--- include/version.h
+++ include/version.h
@@ -2,12 +2,3 @@
 #ifndef RADIANT_VERSION
 #error no RADIANT_VERSION defined
 #endif
-#ifndef RADIANT_MAJOR_VERSION
-#error no RADIANT_MAJOR_VERSION defined
-#endif
-#ifndef RADIANT_MINOR_VERSION
-#error no RADIANT_MINOR_VERSION defined
-#endif
-#ifndef RADIANT_PATCH_VERSION
-#error no RADIANT_PATCH_VERSION defined
-#endif
