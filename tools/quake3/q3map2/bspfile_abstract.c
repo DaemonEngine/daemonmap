5b33d0e7a6312e9bade72f9bfde5e51a6ec60f5d
Thomas Debesse <dev@illwieckz.net>
2020-03-29 01:28:29 +0100

    q3map2: pad with zero, not with null pointers, ref #160
    see:
    
    https://gitlab.com/xonotic/netradiant/-/merge_requests/160#note_287938815

--- tools/quake3/q3map2/bspfile_abstract.c
+++ tools/quake3/q3map2/bspfile_abstract.c
@@ -344,7 +344,7 @@ void AddLump( FILE *file, bspHeader_t *header, int lumpNum, const void *data, in
 	SafeWrite( file, data, length );
 
 	/* write padding zeros */
-	char *zeros[3] = { 0, 0, 0 };
+	char zeros[3] = { 0, 0, 0 };
 	SafeWrite( file, zeros, ( ( length + 3 ) & ~3 ) - length );
 }
 
