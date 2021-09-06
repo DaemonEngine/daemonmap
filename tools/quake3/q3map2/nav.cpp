47002440b3b2b01b2761f2d2f1bf0f138ee36950
Thomas Debesse <dev@illwieckz.net>
2021-09-06 23:22:58 +0200

    nav: rewrite solid and clip detection and surface skip

--- tools/quake3/q3map2/nav.cpp
+++ tools/quake3/q3map2/nav.cpp
@@ -203,23 +203,24 @@ static void AddTri( std::vector<float> &verts, std::vector<int> &tris, vec3_t v1
 static void LoadBrushTris( std::vector<float> &verts, std::vector<int> &tris ) {
 	int j;
 
-	int solidFlags = 0;
-	int temp = 0;
-	int surfaceSkip = 0;
-
 	char surfaceparm[16];
 
+	int defaultFlag = 0;
 	strcpy( surfaceparm, "default" );
-	ApplySurfaceParm( surfaceparm, &solidFlags, NULL, NULL );
+	ApplySurfaceParm( surfaceparm, &defaultFlag, NULL, NULL );
 
+	int playerclipFlag = 0;
 	strcpy( surfaceparm, "playerclip" );
-	ApplySurfaceParm( surfaceparm, &temp, NULL, NULL );
-	solidFlags |= temp;
+	ApplySurfaceParm( surfaceparm, &playerclipFlag, NULL, NULL );
 
+	int skyFlag = 0;
 	if ( excludeSky ) {
 		strcpy( surfaceparm, "sky" );
-		ApplySurfaceParm( surfaceparm, NULL, &surfaceSkip, NULL );
+		ApplySurfaceParm( surfaceparm, NULL, &skyFlag, NULL );
 	}
+
+	int solidFlags = defaultFlag | playerclipFlag;
+	int surfaceSkip = skyFlag;
 
 	/* get model, index 0 is worldspawn entity */
 	bspModel_t *model = &bspModels[0];
@@ -238,6 +239,7 @@ static void LoadBrushTris( std::vector<float> &verts, std::vector<int> &tris ) {
 		if ( !( brushShader->contentFlags & solidFlags ) ) {
 			continue;
 		}
+
 		/* walk the list of brush sides */
 		for ( int p = 0; p < numSides; p++ )
 		{
@@ -298,17 +300,18 @@ static qboolean BoundsIntersect( const vec3_t mins, const vec3_t maxs, const vec
 static void LoadPatchTris( std::vector<float> &verts, std::vector<int> &tris ) {
 
 	vec3_t mins, maxs;
-	int solidFlags = 0;
-	int temp = 0;
 
 	char surfaceparm[16];
 
+	int defaultFlag = 0;
 	strcpy( surfaceparm, "default" );
-	ApplySurfaceParm( surfaceparm, &solidFlags, NULL, NULL );
+	ApplySurfaceParm( surfaceparm, &defaultFlag, NULL, NULL );
 
+	int playerclipFlag = 0;
 	strcpy( surfaceparm, "playerclip" );
-	ApplySurfaceParm( surfaceparm, &temp, NULL, NULL );
-	solidFlags |= temp;
+	ApplySurfaceParm( surfaceparm, &playerclipFlag, NULL, NULL );
+
+	int solidFlags = defaultFlag | playerclipFlag;
 
 	/*
 	    Patches are not used during the bsp building process where
